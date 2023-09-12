cccc5993488fdf38f7abeafb4dd7dfd7476241c0
fkaranikas15 <filipostree95@gmail.com>
2023-09-12 20:18:58 +0300

    Initial commit

--- src/features/contact/ContactForm.jsx
+++ src/features/contact/ContactForm.jsx
@@ -3,8 +3,23 @@ import Form from "../form/Form";
 import FormRow from "../form/FormRow";
 import Input from "../form/Input";
 import { toast } from "react-hot-toast";
-import Textarea from "../form/TextArea";
 import { Button } from "../../ui/Button";
+import styled from "styled-components";
+
+const Textarea = styled.textarea`
+  padding: 1.2rem 2.4rem;
+  border: 1px solid var(--color-primary);
+  border-radius: 1rem;
+  background-color: var(--color-white);
+  box-shadow: var(--box-shadow-light);
+  width: 100%;
+  height: 10rem;
+  margin-bottom: 2rem;
+
+  &:focus {
+    outline: 1px solid var(--color-primary);
+  }
+`;
 
 function ContactForm() {
   const { register, handleSubmit, reset, formState } = useForm();
